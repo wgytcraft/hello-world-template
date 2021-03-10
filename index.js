737f8f5e656bd8880e6d29d0703c02608e28f7d5
‍ <williamhorning@wgyt.tk>
2021-03-10 21:36:46 +0000

    config update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function(host, res, req, error, version, ejs){
+module.exports = function(host, res, req, error, version, ejs,config){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write('hello world!')
 	res.end();
